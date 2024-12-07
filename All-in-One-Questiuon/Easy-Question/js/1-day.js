63aa1cd61627667a7be72a699d1fd24065e400ae
Adanan Chouhan <adananchouhan@gmail.com>
2024-12-07 23:12:12 +0530

    Edabit Quetion

--- All-in-One-Questiuon/Easy-Question/js/1-day.js
+++ All-in-One-Questiuon/Easy-Question/js/1-day.js
@@ -159,4 +159,16 @@ console.log(reverseWords(" the sky is blue")); // "blue is sky the"
 console.log(reverseWords("hello   world!  ")); // "world! hello"
 console.log(reverseWords("a good example")); // "example good a"
 
+// function  reverseWords(str){
+//   let result = [];
+// let words = str.split(" ");
+// for(let i = words.length-1; i >= 0; i--){
+// result.push(words[i]);
+// }
+// return result.join(" ").trim();
+// }
+
+// console.log(reverseWords(" the sky is blue")); // "blue is sky the"
+// console.log(reverseWords("hello   world!  ")); // "world! hello"
+// console.log(reverseWords("a good example")); // "example good a"
 
