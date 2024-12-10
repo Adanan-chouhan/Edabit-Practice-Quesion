822ec2dd686356e464ca1fb0a6695ef5abd169ca
Adanan Chouhan <adananchouhan@gmail.com>
2024-12-10 22:50:56 +0530

    Edabit Quetion

--- string Quetions/Easy/js/2-day.js
+++ string Quetions/Easy/js/2-day.js
@@ -29,17 +29,49 @@ function removeVowels(str){
 // Create a function that returns the index of the first vowel in a string.
 
 function firstVowel(str){
-    let firstVowel = 0
     let vowels = "aeiouAEIOU";
     for(let i = 0; i < str.length; i++){
-        if(vowels.includes(str[i]).firstINdexOf()){
-          firstVowel++
+        if(vowels.includes(str[i])){
+            return i;
         }
     }
-    return firstVowel;
+    return -1
 }
 
-console.log(firstVowel("apple"));      //  0
-console.log(firstVowel("hello"));      //  1
-console.log(firstVowel("STRAWBERRY"));      //  3
-console.log(firstVowel("pInEaPPLe"));      //  1
+// function firstVowel(str) {
+//     let index = str.search(/[aeiou]/i);
+//     return index;
+// }
+
+// console.log(firstVowel("apple"));      //  0
+// console.log(firstVowel("hello"));      //  1
+// console.log(firstVowel("STRAWBERRY"));      //  3
+// console.log(firstVowel("pInEaPPLe"));      //  1
+
+// 13 => Is it True?
+// In this challenge you will be given a relation between two numbers, written as a string. Write a function that determines if the relation is true or false.
+
+function isTrue(str){
+    let formattedStr = str.replace(/=/g, "==");
+    let result = eval(formattedStr);
+    return result;
+}
+
+// console.log(isTrue("2=2"));    //  true
+// console.log(isTrue("8<7"));    //  false
+// console.log(isTrue("5=13"));    //  false
+// console.log(isTrue("15>4"));    //  true
+
+// 14 => Regex Series: Even Number?
+// Write a regular expression that matches only an even number. Numbers will be presented as strings.
+
+function checkEven(str){
+   let regex = /^[0-9]*[0,2,4,6,8]$/
+   return regex.test(str);
+}
+
+checkEven("2341");   //  false
+checkEven("132");   //  true
+checkEven("29");   //  false
+checkEven("5578");   //  true
+
